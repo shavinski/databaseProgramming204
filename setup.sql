bc3b43176932288be2a5dcd31abcf641c3a665a4
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-03-18 20:48:35 -0700

    Finish creating tables for database

---
+++ setup.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Author (
+    author_id INT PRIMARY KEY,
+    author_first_name VARCHAR(50),
+    author_last_name VARCHAR(50),
+    author_nationality VARCHAR(50)
+);
+
+CREATE TABLE Book (
+    book_id INT PRIMARY KEY,
+    book_title VARCHAR(50),
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES Author(author_id)
+);
+
+CREATE TABLE Client (
+    client_id INT PRIMARY KEY,
+    client_first_name VARCHAR(50),
+    client_last_name VARCHAR(50),
+    client_dob INT,
+    occupation VARCHAR(50)
+);
+
+CREATE TABLE Borrower (
+    borrow_id INT PRIMARY KEY,
+    client_id INT,
+    book_id INT,
+    borrow_date DATE,
+    FOREIGN KEY (client_id) REFERENCES Client(client_id),
+    FOREIGN KEY (book_id) REFERENCES Book(book_id),
+);
