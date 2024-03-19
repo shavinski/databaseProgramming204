25f3c8c831e7f9291d05ad730e6c0e086cd07fac
Jakob Shavinski <shavinski.jakob@gmail.com>
2024-03-18 20:54:40 -0700

    Fix errors in creating tables
    Extra comma in last create table

--- setup.sql
+++ setup.sql
@@ -1,18 +1,18 @@
-CREATE TABLE Author (
+CREATE TABLE IF NOT EXISTS Author (
     author_id INT PRIMARY KEY,
     author_first_name VARCHAR(50),
     author_last_name VARCHAR(50),
     author_nationality VARCHAR(50)
 );
 
-CREATE TABLE Book (
+CREATE TABLE IF NOT EXISTS Book (
     book_id INT PRIMARY KEY,
     book_title VARCHAR(50),
     author_id INT,
     FOREIGN KEY (author_id) REFERENCES Author(author_id)
 );
 
-CREATE TABLE Client (
+CREATE TABLE IF NOT EXISTS Client (
     client_id INT PRIMARY KEY,
     client_first_name VARCHAR(50),
     client_last_name VARCHAR(50),
@@ -20,11 +20,12 @@ CREATE TABLE Client (
     occupation VARCHAR(50)
 );
 
-CREATE TABLE Borrower (
+CREATE TABLE IF NOT EXISTS Borrower (
     borrow_id INT PRIMARY KEY,
     client_id INT,
     book_id INT,
     borrow_date DATE,
     FOREIGN KEY (client_id) REFERENCES Client(client_id),
-    FOREIGN KEY (book_id) REFERENCES Book(book_id),
+    FOREIGN KEY (book_id) REFERENCES Book(book_id)
 );
+
